a2b657a6be00bdc092e1401529f165ea07b929db
Arjun Vikram <arjvik@gmail.com>
2017-06-04 19:11:22 -0500

    Fixed things under the hood

--- src/DEBUG.java
+++ src/DEBUG.java
@@ -1,8 +1,6 @@
-
 class PrintWriter {
-
 	public PrintWriter(Object wildcard) {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	public void println(int i){
@@ -20,4 +18,52 @@ class PrintWriter {
 	public void close(){
 		System.out.println("PrintWriter closed");
 	}
+	
+	public void print(int i){
+		System.out.print(i);
+	}
+	
+	public void print(String s){
+		System.out.print(s);
+	}
+	
+	public void print(Object o){
+		System.out.print(o);
+	}
+}
+
+class Scanner{
+	java.util.Scanner scan;
+	
+	public Scanner(Object wildcard) {
+		scan=new java.util.Scanner(System.in);
+	}
+
+	public int nextInt() {
+		return scan.nextInt();
+	}
+
+	public String nextLine() {
+		return scan.nextLine();
+	}
+	
+	public void close() {
+		scan.close();
+		System.out.println("Scanner closed");
+	}
+}
+
+class BufferedReader{
+
+	public BufferedReader(Object wildcard) {
+		
+	}
+}
+
+class FileReader{
+
+	public FileReader(String string) {
+		
+	}
+	
 }
