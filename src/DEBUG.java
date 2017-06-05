031229de0d66c825dd5b8120b42cf07d072654b9
Arjun Vikram <arjvik@gmail.com>
2017-06-04 19:13:46 -0500

    More changes under the hood

--- src/DEBUG.java
+++ src/DEBUG.java
@@ -2,51 +2,52 @@ class PrintWriter {
 	public PrintWriter(Object wildcard) {
 		
 	}
-	
 	public void println(int i){
 		System.out.println(i);
 	}
-	
 	public void println(String s){
 		System.out.println(s);
 	}
-	
 	public void println(Object o){
 		System.out.println(o);
 	}
-	
 	public void close(){
 		System.out.println("PrintWriter closed");
 	}
-	
 	public void print(int i){
 		System.out.print(i);
 	}
-	
 	public void print(String s){
 		System.out.print(s);
 	}
-	
 	public void print(Object o){
 		System.out.print(o);
 	}
 }
 
+class BufferedWriter{
+	public BufferedWriter(Object wildcard) {
+		
+	}
+}
+
+class FileWriter{
+	public FileWriter(String string) {
+		
+	}
+}
+
 class Scanner{
 	java.util.Scanner scan;
-	
 	public Scanner(Object wildcard) {
 		scan=new java.util.Scanner(System.in);
 	}
-
 	public int nextInt() {
 		return scan.nextInt();
 	}
-
 	public String nextLine() {
 		return scan.nextLine();
 	}
-	
 	public void close() {
 		scan.close();
 		System.out.println("Scanner closed");
@@ -54,16 +55,13 @@ class Scanner{
 }
 
 class BufferedReader{
-
 	public BufferedReader(Object wildcard) {
 		
 	}
 }
 
 class FileReader{
-
 	public FileReader(String string) {
 		
 	}
-	
 }
