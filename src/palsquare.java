06a3ca6f8a21771afd4698a076372b8e59c4ed60
Arjun Vikram <arjvik@gmail.com>
2017-06-21 16:43:03 -0500

    palsquare

---
+++ src/palsquare.java
@@ -0,0 +1,25 @@
+/*
+ID: arjvik1
+LANG: JAVA
+TASK: palsquare
+*/
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+@SuppressWarnings("unused")
+public class palsquare {
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(new BufferedReader(new FileReader("palsquare.in")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("palsquare.out")));
+		int B = in.nextInt();
+		IntStream.rangeClosed(1, 300)
+				.filter(i->{
+					String square = Integer.toString(i*i,B);
+					return new StringBuilder(square).reverse().toString().equals(square);
+				})
+				.forEachOrdered(i->out.println(Integer.toString(i,B).toUpperCase()+" "+Integer.toString(i*i,B).toUpperCase()));in.close();
+		out.close();
+	}
+	
+}
