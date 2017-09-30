2cddc32313423fb18d07799aa1b43949bd257978
Arjun Vikram <arjvik@gmail.com>
2017-09-29 20:51:36 -0500

    numtri

--- Training/numtri.java
+++ Training/numtri.java
@@ -5,52 +5,42 @@ TASK: numtri
 */
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
 
-@SuppressWarnings("unused")
 public class numtri {
-	private static int r;
-	private static int leaf_counter = 0;
-	private static int node_counter;
-	//private static final Set<Integer> squares = Collections.unmodifiableSet(IntStream.range(1, 2002).map(i -> i*i).boxed().collect(Collectors.toSet()));
-													
+
+	public int solve(int n, int[][] triangle) {
+		for (int i = n - 2; i >= 0; i--) {
+			for (int j = 0; j <= i; j++) {
+				triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+			}
+		}
+		return triangle[0][0];
+	}
+
+
 	public static void main(String[] args) throws IOException {
-		long startTime = System.currentTimeMillis();
-		Scanner in = new Scanner(new BufferedReader(new FileReader("numtri.in")));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("numtri.out")));
-		r = in.nextInt();
-		int numElements = (r*(r+1))/2;
-		node_counter = numElements;
-		int[] tree = new int[numElements+1];
-		int[] highest = new int[numElements+1];
-		for (int i = 1; i <= numElements; i++) {
-			tree[i] = in.nextInt();
+		String problemName = "numtri";
+		BufferedReader f = new BufferedReader(new FileReader(problemName + ".in"));
+
+		int n = Integer.parseInt(f.readLine());
+		
+		int[][] triangle = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			triangle[i] = new int[n];
+			StringTokenizer st = new StringTokenizer(f.readLine());
+			for (int j = 0; j <= i; j++) {
+				triangle[i][j] = Integer.parseInt( st.nextToken() );
+			}
 		}
-		for (int n = numElements; n > 0; n--) {
-			int child = getChild(n);
-			if(child == -1)
-				highest[n]=tree[n];
-			else
-				highest[n]=tree[n]+Math.max(highest[child],highest[child+1]);
-		}
-		out.println(highest[1]);
-		in.close();
-		out.close();
-		System.out.format("Took %.3f seconds",((System.currentTimeMillis()-startTime)/1000d));
+
+		int res = (new numtri()).solve(n, triangle);
+
+		// output Span
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(problemName + ".out")));
+			out.println(res);
+		out.close(); // close the output file
+		System.exit(0); // don't omit this!
 	}
-	private static int getChild(int i){
-		if(leaf_counter<r){
-			leaf_counter++;
-			return -1;
-		}
-		node_counter--;
-		if(isTriangular(node_counter))
-			node_counter--;
-		return node_counter;
-	}
-	private static boolean isTriangular(int n) { //https://stackoverflow.com/questions/2913215/fastest-method-to-define-whether-a-number-is-a-triangular-number
-		int n1 = (8*n)+1;
-		int tst = (int)(Math.sqrt(n1) + 0.5);
-		return tst*tst == n1;
-	}
+
+
 }
---
+++ Training/numtri_suboptimal.java
@@ -0,0 +1,56 @@
+/*
+ID: arjvik1
+LANG: JAVA
+TASK: numtri
+*/
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+@SuppressWarnings("unused")
+public class numtri_suboptimal {
+	private static int r;
+	private static int leaf_counter = 0;
+	private static int node_counter;
+	//private static final Set<Integer> squares = Collections.unmodifiableSet(IntStream.range(1, 2002).map(i -> i*i).boxed().collect(Collectors.toSet()));
+													
+	public static void main(String[] args) throws IOException {
+		long startTime = System.currentTimeMillis();
+		Scanner in = new Scanner(new BufferedReader(new FileReader("numtri.in")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("numtri.out")));
+		r = in.nextInt();
+		int numElements = (r*(r+1))/2;
+		node_counter = numElements;
+		int[] tree = new int[numElements+1];
+		int[] highest = new int[numElements+1];
+		for (int i = 1; i <= numElements; i++) {
+			tree[i] = in.nextInt();
+		}
+		for (int n = numElements; n > 0; n--) {
+			int child = getChild(n);
+			if(child == -1)
+				highest[n]=tree[n];
+			else
+				highest[n]=tree[n]+Math.max(highest[child],highest[child+1]);
+		}
+		out.println(highest[1]);
+		in.close();
+		out.close();
+		System.out.format("Took %.3f seconds",((System.currentTimeMillis()-startTime)/1000d));
+	}
+	private static int getChild(int i){
+		if(leaf_counter<r){
+			leaf_counter++;
+			return -1;
+		}
+		node_counter--;
+		if(isTriangular(node_counter))
+			node_counter--;
+		return node_counter;
+	}
+	private static boolean isTriangular(int n) { //https://stackoverflow.com/questions/2913215/fastest-method-to-define-whether-a-number-is-a-triangular-number
+		int n1 = (8*n)+1;
+		int tst = (int)(Math.sqrt(n1) + 0.5);
+		return tst*tst == n1;
+	}
+}
