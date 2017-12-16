abc9878aec9f8a49fe76b1927ad608f8dd62b3e8
Arjun Vikram <arjvik@gmail.com>
2017-12-16 16:36:26 -0600

    measurement (slow)

---
+++ Dec2017/measurementSlow.java
@@ -0,0 +1,93 @@
+/*
+ID: arjvik1
+LANG: JAVA
+TASK: measurement
+*/
+import java.io.*;
+import java.util.*;
+
+public class measurementSlow {
+	public static class Entry implements Comparable<Entry>{
+		int date;
+		int name;
+		int addition;
+		public Entry() {
+			
+		}
+		public Entry(int date, int name, int addition) {
+			this();
+			this.date = date;
+			this.name = name;
+			this.addition = addition;
+		}
+		@Override
+		public int compareTo(Entry o) {
+			return Integer.compare(date, o.date);
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(new BufferedReader(new FileReader("measurement.in")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("measurement.out")));
+		int lines = in.nextInt();
+		int g = in.nextInt();
+		List<Entry> entries = new ArrayList<>();
+		Set<Integer> leaderboard = new HashSet<>();
+		Map<Integer,Integer> output = new HashMap<>();
+		output.put(-1, g);
+		leaderboard.add(-1);
+		for (int i = 0; i < lines; i++) {
+			int date = in.nextInt();
+			int name = in.nextInt();
+			int ammt = in.nextInt();
+			entries.add(new Entry(date,name,ammt));
+			output.put(name, g);
+			leaderboard.add(name);
+		}
+		entries.sort(null);		
+		int count = 0;
+		for (Entry e : entries) {
+			output.put(e.name,output.get(e.name)+e.addition);
+			Set<Integer> newLeaderboard = getLeaderboard(output);
+			if(!leaderboard.equals(newLeaderboard)){
+				count++;
+				leaderboard = newLeaderboard;
+			}
+			/*if(leaderboard.contains(e.name)){
+				if(e.addition==0)
+					continue loop;
+				if(e.addition>0){
+					count++;
+					leaderboard.clear();
+					leaderboard.add(e.name);
+				}else{
+					count++;
+					leaderboard.remove(e.name);
+				}
+			}else{
+				Set<Integer> newLeaderboard = getLeaderboard(output);
+				if(!leaderboard.equals(newLeaderboard)){
+					count++;
+					leaderboard = newLeaderboard;
+				}
+			}*/
+		}
+		
+		out.println(count);
+		in.close();
+		out.close();
+	}
+	private static Set<Integer> getLeaderboard(Map<Integer, Integer> output) {
+		int max = Integer.MIN_VALUE;
+		Set<Integer> ans = null;
+		for (Map.Entry<Integer,Integer> e : output.entrySet()) {
+			if(e.getValue()> max){
+				max = e.getValue();
+				ans = new HashSet<>();
+				ans.add(e.getKey());
+			}else if(e.getValue()==max){
+				ans.add(e.getKey());
+			}
+		}
+		return ans;
+	}
+}
