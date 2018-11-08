18ab171083eddcb4274560e34867965a23c8d7ed
Arjun Vikram <arjvik@gmail.com>
2018-11-07 22:39:38 -0600

    optimized snowboots (Still a WIP)

--- USACO/Feb2018/snowboots.java
+++ USACO/Feb2018/snowboots.java
@@ -1,3 +1,4 @@
+
 /*
 ID: arjvik1
 LANG: JAVA
@@ -6,40 +7,53 @@ TASK: snowboots
 import java.io.*;
 import java.util.*;
 
-
 @SuppressWarnings("unused")
 public class snowboots {
-	private static int[] depth;
-//	private static int[] max_forward;
+	private static int[] depth, b_depth, b_dist;
+
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(new BufferedReader(new FileReader("snowboots.in")));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("snowboots.out")));
 		int n = in.nextInt();
 		int b = in.nextInt();
 		depth = new int[n];
-//		max_forward = new int[n];
-		for(int i = 0; i < n; i++){
-			int d = in.nextInt();
-			depth[i] = d;
-//			for (int j = 0; j <= i; j++)
-//				if(max_forward[j] < d)
-//					max_forward[j] = d;
-		}
+		b_depth = new int[n];
+		b_dist = new int[n];
+		for (int i = 0; i < n; i++)
+			depth[i] = in.nextInt();
+		Map<Integer, Integer> depth_dist = new TreeMap<>();
 		for (int i = 0; i < b; i++) {
-			out.println(run(in.nextInt(),in.nextInt(),0)?1:0);
+			b_depth[i] = in.nextInt();
+			b_dist[i] = in.nextInt();
+			depth_dist.put(b_depth[i], -1);
 		}
+		for (int bd : depth_dist.keySet()) {
+			int max_stretch = getMaxStretch(bd);
+			depth_dist.put(bd, max_stretch + 1);
+		}
+		for (int i = 0; i < b; i++)
+			out.println((depth_dist.get(b_depth[i]) > b_dist[i]) ? 0 : 1);
 		
 		in.close();
 		out.close();
 	}
-	private static boolean run(int max_depth, int step_size, int pos /** 0-based index **/) {
-		if(pos>=depth.length-1)
-			return true;
-//		if(max_forward[pos]<=max_depth)
-//			return true;
-		for (int i = step_size; i > 0; i--)
-			if(pos+i<depth.length && depth[pos+i]<=max_depth && run(max_depth, step_size, pos+i))
-				return true;
-		return false;
+
+	private static int getMaxStretch(int bd) {
+		// the longest stretch of depths greater than bd
+		int maxStretch = 0;
+		int stretch = 0;
+		for(int i = 0; i < depth.length; i++) {
+			if(depth[i] > bd) {
+				stretch++;
+			} else {
+				if(stretch > maxStretch)
+					maxStretch = stretch;
+				stretch = 0;
+			}
+		}
+		if(stretch > maxStretch)
+			maxStretch = stretch;
+		return maxStretch;
 	}
+
 }
