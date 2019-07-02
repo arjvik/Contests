edc4bfbb75a66db43bbddcc3b66d5affccde8863
Arjun Vikram <arjvik@gmail.com>
2019-07-02 08:32:47 -0500

    Codeforces: The Fair Nut and String - not even a dp problem, quite easy

---
+++ Codeforces/TheFairNutAndString.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+@SuppressWarnings("unused")
+public class TheFairNutAndString {
+	public static long MOD = (long) (1e9+7);
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine().replaceAll("[^ab]", "");
+		long x = 0, b = 0;
+		for (char c : s.toCharArray())
+			if (c == 'a')
+				x = mod(x + b + 1);
+			else
+				b = x;
+		System.out.println(x);
+	}
+	public static long mod(long l) {
+		return l % MOD;
+	}
+}
