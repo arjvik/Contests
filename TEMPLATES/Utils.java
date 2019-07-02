ad3dfa381f1a22fec04314939785667e06b20978
Arjun Vikram <arjvik@gmail.com>
2019-07-02 08:11:15 -0500

    Added array io helper methods to Utils class

--- TEMPLATES/Utils.java
+++ TEMPLATES/Utils.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
 /**
 Utility class containing many useful methods
 
@@ -20,4 +25,61 @@ public class Utils {
 				array[i][j] = in.nextInt();
 		return array;
 	}
+	
+	public static long[] readArrayLong(Scanner in, int n) {
+		long[] array = new long[n];
+		for (int i = 0; i < n; i++)
+			array[i] = in.nextLong();
+		return array;
+	}
+
+	public static long[][] readArray2dLong(Scanner in, int n, int m) {
+		long[][] array = new long[n][m];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				array[i][j] = in.nextLong();
+		return array;
+	}
+	
+	public static <T> void printArray(T[] array) {
+		System.out.println(Arrays.asList(array));
+	}
+	
+	public static <T> void printArray2d(T[][] array) {
+		System.out.println("{");
+		for (T[] row : array) {
+			System.out.print("\t");
+			printArray(row);
+			System.out.println(",");
+		}
+		System.out.println("}");
+	}
+	
+	public static void printArray(int[] array) {
+		System.out.println(IntStream.of(array).mapToObj(String::valueOf).collect(Collectors.joining(",", "[", "]")));
+	}
+	
+	public static void printArray2d(int[][] array) {
+		System.out.println("{");
+		for (int[] row : array) {
+			System.out.print("\t");
+			printArray(row);
+			System.out.println(",");
+		}
+		System.out.println("}");
+	}
+	
+	public static void printArray(long[] array) {
+		System.out.println(LongStream.of(array).mapToObj(String::valueOf).collect(Collectors.joining(",", "[", "]")));
+	}
+	
+	public static void printArray2d(long[][] array) {
+		System.out.println("{");
+		for (long[] row : array) {
+			System.out.print("\t");
+			printArray(row);
+		}
+		System.out.println("}");
+	}
+
 }
